2245d5b865da741ec21cf9fc16a44810306b3c54
Saif Ali <saif5969396@gmail.com>
2023-08-31 10:57:46 -0700

    WeatherFrecastApp

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:weatherforecast/weather_forecast/ui.dart';
 void main(){
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: WeatherForeCast(),
   ));
 }
